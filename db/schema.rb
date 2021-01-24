2c04fcb7ed67b786e267c1648ae2f817f3637784
EC2 Default User <m45.alcy1@outlook.jp>
2021-01-24 09:12:45 +0000

    [Fix]error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_035858) do
+ActiveRecord::Schema.define(version: 2021_01_24_062422) do
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_doctors_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_doctors_on_reset_password_token", unique: true
+  end
 
   create_table "notices", force: :cascade do |t|
     t.string "title"
@@ -18,17 +30,6 @@ ActiveRecord::Schema.define(version: 2021_01_24_035858) do
     t.string "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "reservations", force: :cascade do |t|
-    t.integer "doctor_id"
-    t.integer "user_id"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["doctor_id"], name: "index_reservations_on_doctor_id"
-    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
 end
