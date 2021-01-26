9c5a850dcb4e69a7f68c8341c039dfe7095ca7fe
EC2 Default User <m45.alcy1@outlook.jp>
2021-01-26 08:35:33 +0000

    [Add]hotel html

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  
   root to: "homes#home"
   get "/trimming" => "homes#trimming"
-  
+  get "/hotel" => "homes#hotel"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
